6cf95666ba60f3b07bce268b7d76a8f6008c96fa
mildrette <mildredfonka1@gmail.com>
2022-07-26 12:08:43 +0100

    added map using arreys

--- Mildred_bar_Chart/script.js
+++ Mildred_bar_Chart/script.js
@@ -1,8 +1,42 @@
 google.charts.load('current', {'packages':['bar']});
             google.charts.setOnLoadCallback(drawStuff);
       
+          
             function drawStuff() {
-              var data = new google.visualization.arrayToDataTable([
+    const entry = [
+        {Month: 'Month', Day: 'Days'},
+        {Month: 'Jaunary', Day: 31},
+        {Month: 'Faburay', Day: 28},
+        {Month: 'March', Day: 31},
+        {Month: 'April', Day: 30}, 
+        {Month: 'May', Day: 31},
+        {Month: 'June', Day: 30},
+        {Month: 'July', Day: 31},
+        {Month: 'August', Day: 31},
+        {Month: 'September', Day: 30},
+        {Month: 'October', Day: 31},
+        {Month: 'Nevember', Day: 30},
+        {Month: 'December', Day: 31},
+  ];
+
+  // Create the data table.
+  // Call data using map
+  var data = google.visualization.arrayToDataTable (
+      entry.map(function(element){
+          result = []
+          result.push(element.Month)
+          result.push(element.Day)
+          return result;
+      })
+  );
+
+  // Ticks 
+  var vAxisRange = data.getColumnRange(1);
+  var ticks = [];
+  for (var i = 0; i <= vAxisRange.max; i++) {
+      ticks.push(i);
+  }    
+                /*[
                 ['Months', 'Numbers of days'],
                 ["Jaunary", 31],
                 ["Faburay", 28],
@@ -16,7 +50,7 @@ google.charts.load('current', {'packages':['bar']});
                 ["October", 31],
                 ["Nevember", 30],
                 ["December", 31],
-                ]);
+                ]);*/
 
 
               var options = {
