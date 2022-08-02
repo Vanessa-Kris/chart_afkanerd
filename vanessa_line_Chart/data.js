3156ab60cd7706e24fcba44ae05e1640e4576691
Minion <nessakristo@gmail.com>
2022-08-03 00:16:48 +0100

    filter by month type

---
+++ vanessa_line_Chart/data.js
@@ -0,0 +1,114 @@
+const entry = [{
+        Month: 'Months',
+        Days: 'Days',
+        Orange: 'Orange subscribers',
+        Camtel: 'Camtel Subscribers',
+        MTN: 'MTN Subscribers',
+        Nextel: 'Nextel Subscribers'
+    },
+    {
+        Month: 'Jan',
+        Days: 31,
+        Orange: 20,
+        Camtel: 24,
+        MTN: 0,
+        Nextel: 10
+    },
+    {
+        Month: 'Feb',
+        Days: 28,
+        Orange: 20,
+        Camtel: 9,
+        MTN: 6,
+        Nextel: 30
+    },
+    {
+        Month: 'Mar',
+        Days: 31,
+        Orange: 10,
+        Camtel: 15,
+        MTN: 8,
+        Nextel: 20
+    },
+    {
+        Month: 'Apr',
+        Days: 30,
+        Orange: 20,
+        Camtel: 6,
+        MTN: 9,
+        Nextel: 31
+    },
+    {
+        Month: 'May',
+        Days: 31,
+        Orange: 22,
+        Camtel: 1,
+        MTN: 12,
+        Nextel: 12
+    },
+    {
+        Month: 'Jun',
+        Days: 30,
+        Orange: 20,
+        Camtel: 0,
+        MTN: 20,
+        Nextel: 14
+    },
+    {
+        Month: 'Jul',
+        Days: 31,
+        Orange: 20,
+        Camtel: 21,
+        MTN: 25,
+        Nextel: 16
+    },
+    {
+        Month: 'Aug',
+        Days: 31,
+        Orange: 20,
+        Camtel: 12,
+        MTN: 0,
+        Nextel: 20
+    },
+    {
+        Month: 'Sep',
+        Days: 30,
+        Orange: 13,
+        Camtel: 3,
+        MTN: 20,
+        Nextel: 30
+    },
+    {
+        Month: 'Oct',
+        Days: 31,
+        Orange: 20,
+        Camtel: 10,
+        MTN: 20,
+        Nextel: 31
+    },
+    {
+        Month: 'Nov',
+        Days: 30,
+        Orange: 20,
+        Camtel: 0,
+        MTN: 20,
+        Nextel: 10
+    },
+    {
+        Month: 'Dec',
+        Days: 31,
+        Orange: 40,
+        Camtel: 3,
+        MTN: 20,
+        Nextel: 6
+    }
+    
+];
+
+// function my1stFunction(); {
+//     let month_type = document.getElementById("months").value;
+//             if (month_type == "28") {
+//                 result = entry.map[2];
+// }
+//}
+
