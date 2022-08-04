a5ebfba75e587180a69c8125a034908b307d1494
mildrette <mildredfonka1@gmail.com>
2022-08-04 11:22:28 +0100

    remove network operators

--- Mildred_bar-Chart/Main.js
+++ Mildred_bar-Chart/Main.js
@@ -13,21 +13,78 @@ function my1stFunction() {
         var result = []
         result.push([
             entry[0].Month,
-            entry[0].Nextel,
-            entry[0].Orange,
-            entry[0].MTN,
-            entry[0].Camtel
+            entry[0].Days
+            //entry[0].Orange,
+            //entry[0].MTN,
+            //entry[0].Camtel
         ])
 
         entry.map(function (element) {
             if(element.Days == 28){
                 data = []
                 data.push(element.Month)
-                data.push(element.Nextel)
-                data.push(element.Orange)
-                data.push(element.MTN)
-                data.push(element.Camtel)
+                data.push(element.Days)
+               // data.push(element.Orange)
+              // data.push(element.MTN)
+                //data.push(element.Camtel)
+                result.push(data);
+            }
+        })
+       
+        var data = google.visualization.arrayToDataTable(
+            result
+        );
 
+
+        // Set chart options
+        var options = {
+            vAxis: {
+                title: 'Days',
+                format: '0',
+            },
+            hAxis: {
+                title: 'Months'
+            },
+            'title': 'Daily Subscribers Visualization',
+
+        };
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+                 chart.draw(data, options);
+       
+    }
+}
+
+
+
+
+// 29 Days
+function my2stFunction() {
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var result = []
+        result.push([
+            entry[0].Month,
+            entry[0].Days
+            //entry[0].Orange,
+            //entry[0].MTN,
+            //entry[0].Camtel
+        ])
+
+        entry.map(function (element) {
+            if(element.Days == 29){
+                data = []
+                data.push(element.Month)
+                data.push(element.Days)
+               // data.push(element.Orange)
+              // data.push(element.MTN)
+                //data.push(element.Camtel)
                 result.push(data);
             }
         })
@@ -58,7 +115,7 @@ function my1stFunction() {
 
 
 // 30 Days
-function my2ndFunction() {
+function my3ndFunction() {
     google.charts.load('current', {
         'packages': ['corechart']
     });
@@ -71,21 +128,20 @@ function my2ndFunction() {
         var result = []
         result.push([
             entry[0].Month,
-            entry[0].Nextel,
-            entry[0].Orange,
-            entry[0].MTN,
-            entry[0].Camtel
+            entry[0].Days
+            //entry[0].Orange,
+            //entry[0].MTN,
+           // entry[0].Camtel
         ])
 
         entry.map(function (element) {
             if(element.Days == 30){
                 data = []
                 data.push(element.Month)
-                data.push(element.Nextel)
-                data.push(element.Orange)
-                data.push(element.MTN)
-                data.push(element.Camtel)
-
+                data.push(element.Days)
+                //data.push(element.Orange)
+               // data.push(element.MTN)
+                //data.push(element.Camtel)
                 result.push(data);
             }
         })
@@ -113,10 +169,17 @@ function my2ndFunction() {
                  chart.draw(data, options);
        
     }
-}
+};
+
+
+
+
+
+
+
 
 // 31 Days
-function my3rdFunction() {
+function my4rdFunction() {
     google.charts.load('current', {
         'packages': ['corechart']
     });
@@ -129,21 +192,20 @@ function my3rdFunction() {
         var result = []
         result.push([
             entry[0].Month,
-            entry[0].Nextel,
-            entry[0].Orange,
-            entry[0].MTN,
-            entry[0].Camtel
+            entry[0].Days
+            //entry[0].Orange,
+            //entry[0].MTN,
+            //entry[0].Camtel
         ])
 
         entry.map(function (element) {
             if(element.Days == 31){
                 data = []
                 data.push(element.Month)
-                data.push(element.Nextel)
-                data.push(element.Orange)
-                data.push(element.MTN)
-                data.push(element.Camtel)
-
+                data.push(element.Days)
+                //data.push(element.Orange)
+                //data.push(element.MTN)
+                //data.push(element.Camtel)
                 result.push(data);
             }
         })
@@ -171,4 +233,4 @@ function my3rdFunction() {
                  chart.draw(data, options);
        
     }
-}
+};
--- Mildred_bar-Chart/script.js
+++ Mildred_bar-Chart/script.js
@@ -1,106 +1,114 @@
 const entry = [{
         Month: 'Months',
-        Days: 'Days',
-        Orange: 'Orange subscribers',
-        Camtel: 'Camtel Subscribers',
-        MTN: 'MTN Subscribers',
-        Nextel: 'Nextel Subscribers'
+        Days: 'Days'
+        //Orange: 'Orange subscribers',
+       // Camtel: 'Camtel Subscribers',
+      //  MTN: 'MTN Subscribers',
+     //   Nextel: 'Nextel Subscribers'
     },
     {
-        Month: 'Jan',
-        Days: 31,
-        Orange: 20,
-        Camtel: 24,
-        MTN: 0,
-        Nextel: 10
+        Month: 'January',
+        Days: 31
+      //  Orange: 20,
+        //Camtel: 24,
+       // MTN: 0,
+        //Nextel: 10
     },
     {
-        Month: 'Feb',
-        Days: 28,
-        Orange: 20,
-        Camtel: 9,
-        MTN: 6,
-        Nextel: 30
+        Month: 'Febuary',
+        Days: 28
+      //  Orange: 20,
+      //  Camtel: 9,
+       // MTN: 6,
+     //   Nextel: 30
+    },
+   {
+       Month: 'Febuary',
+       Days: 29
+      //  Orange: 20,
+      //  Camtel: 9,
+       // MTN: 6,
+     //   Nextel: 30
+   },
+    {
+        Month: 'March',
+        Days: 31
+       // Orange: 10,
+       // Camtel: 15,
+       // MTN: 8,
+       // Nextel: 20
     },
     {
-        Month: 'Mar',
-        Days: 31,
-        Orange: 10,
-        Camtel: 15,
-        MTN: 8,
-        Nextel: 20
-    },
-    {
-        Month: 'Apr',
-        Days: 30,
-        Orange: 20,
-        Camtel: 6,
-        MTN: 9,
-        Nextel: 31
+        Month: 'April',
+        Days: 30
+        //Orange: 20,
+       // Camtel: 6,
+       // MTN: 9,
+      //  Nextel: 31
     },
     {
         Month: 'May',
-        Days: 31,
-        Orange: 22,
-        Camtel: 1,
-        MTN: 12,
-        Nextel: 12
+        Days: 31
+      //  Orange: 22,
+      //  Camtel: 1,
+      //  MTN: 12,
+      //  Nextel: 12
     },
     {
-        Month: 'Jun',
-        Days: 30,
-        Orange: 20,
-        Camtel: 0,
-        MTN: 20,
-        Nextel: 14
+        Month: 'June',
+        Days: 30
+      //  Orange: 20,
+        //Camtel: 0,
+       // MTN: 20,
+      //  Nextel: 14
     },
     {
-        Month: 'Jul',
-        Days: 31,
-        Orange: 20,
-        Camtel: 21,
-        MTN: 25,
-        Nextel: 16
+        Month: 'July',
+        Days: 31
+        //Orange: 20,
+       // Camtel: 21,
+        //MTN: 25,
+     //   Nextel: 16
     },
     {
-        Month: 'Aug',
-        Days: 31,
-        Orange: 20,
-        Camtel: 12,
-        MTN: 0,
-        Nextel: 20
+        Month: 'August',
+        Days: 31
+       // Orange: 20,
+       // Camtel: 12,
+       // MTN: 0,
+       // Nextel: 20
     },
     {
-        Month: 'Sep',
-        Days: 30,
-        Orange: 13,
-        Camtel: 3,
-        MTN: 20,
-        Nextel: 30
+        Month: 'September',
+        Days: 30
+        //Orange: 13,
+        //Camtel: 3,
+        //MTN: 20,
+        //Nextel: 30
     },
     {
-        Month: 'Oct',
-        Days: 31,
-        Orange: 20,
-        Camtel: 10,
-        MTN: 20,
-        Nextel: 31
+        Month: 'October',
+        Days: 31
+       // Orange: 20,
+        //Camtel: 10,
+        //MTN: 20,
+        //Nextel: 31
     },
     {
-        Month: 'Nov',
-        Days: 30,
-        Orange: 20,
-        Camtel: 0,
-        MTN: 20,
-        Nextel: 10
+        Month: 'November',
+        Days: 30
+        //Orange: 20,
+        //Camtel: 0,
+        //MTN: 20,
+        //Nextel: 10
     },
     {
-        Month: 'Dec',
-        Days: 31,
-        Orange: 40,
-        Camtel: 3,
-        MTN: 20,
-        Nextel: 6
+        Month: 'December',
+        Days: 31
+        //Orange: 40,
+       // Camtel: 3,
+       // MTN: 20,
+        //Nextel: 6
     }
     
 ];
