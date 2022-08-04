e5a4a948947a14c27b84ac60befc58cc157d212f
Minion <nessakristo@gmail.com>
2022-08-04 18:08:36 +0100

    added default chart

--- vanessa_line_Chart/vanessa.js
+++ vanessa_line_Chart/vanessa.js
@@ -1,3 +1,49 @@
+// Default
+
+google.charts.load('current', {
+    'packages': ['corechart']
+});
+
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable(
+        entry.map(function (element) {
+            result = []
+            result.push(element.Month)
+            result.push(element.Days)
+            return result;
+        })
+
+    );
+
+
+    // Set chart options
+    var options = {
+        vAxis: {
+            title: 'Days',
+            format: '0',
+
+        },
+        hAxis: {
+            title: 'Months'
+        },
+        'title': 'Monthly Subscribers Visualization',
+        width: 900,
+        height: 500,
+
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+
+}
+
+
 // 28 Days
 function my1stFunction() {
     google.charts.load('current', {
